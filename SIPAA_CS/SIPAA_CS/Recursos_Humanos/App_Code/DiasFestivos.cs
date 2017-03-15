cd47ab0103bdfbcb65993921c5295cad9f0aef75
bhuizar <bhuizar@BHUIZAR-PC2>
2017-03-15 11:08:46 -0600

    Se agrega la Clase Dias Festivos y Formularios Dias Festivos

---
+++ SIPAA_CS/SIPAA_CS/Recursos_Humanos/App_Code/DiasFestivos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIPAA_CS.Recursos_Humanos.App_Code
+{
+    class DiasFestivos
+    {
+
+        //SE DECLARAN VARIABLES
+        public int vOpcion;
+        public string sResp;
+
+    }
+}
