c80ebaa471815eafd28cd653324eee03383e1b54
Gama\Gamaliel <gamaliellobato@gmail.com>
2017-03-16 20:10:53 -0600

    fix en gitignore

---
+++ SIPAA_CS/SIPAA_CS/Recursos_Humanos/Administracion/Empleados.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using SIPAA_CS.Properties;
+using SIPAA_CS.Recursos_Humanos.App_Code;
+
+namespace SIPAA_CS.Recursos_Humanos.Administracion
+{
+
+    #region variables
+
+
+    #endregion
+
+    public partial class Empleados : Form
+    {
+        public Empleados()
+        {
+            InitializeComponent();
+        }
+        EmpleadosSonarh contenedorempleados = new EmpleadosSonarh();
+
+        //boton buscar empleados
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            //llena grid
+            if (txtEmpleado.Text == String.Empty)
+            {
+                fgridEmpleados(1, txtEmpleado.Text.Trim());
+                txtEmpleado.Text = "";
+                txtEmpleado.Focus();
+            }
+            else
+            {
+                if (radioBtnnempleado.Checked== true & radioBtnapaterno.Checked == false & radioBtnAmbos.Checked==true)
+                {
+                    fgridEmpleados(1, txtEmpleado.Text.Trim());
+                    txtEmpleado.Text = "";
+                    txtEmpleado.Focus();
+                }
+                else
+                {
+                    if (radioBtnnempleado.Checked == false & radioBtnapaterno.Checked == true & radioBtnAmbos.Checked == true)
+                    {
+                        fgridEmpleados(2, txtEmpleado.Text.Trim());
+                        txtEmpleado.Text = "";
+                        txtEmpleado.Focus();
+                    }
+                }
+            }
+            //Conbinacion de consultas sencillas
+            if (radioBtnnempleado.Checked== true & radioBtnActivo.Checked==true)
+            {
+                fgridEmpleados(3, txtEmpleado.Text.Trim());
+                txtEmpleado.Text = "";
+                txtEmpleado.Focus();
+            }
+
+            if (radioBtnnempleado.Checked == true & radioBtnInactivo.Checked == true)
+            {
+                fgridEmpleados(4, txtEmpleado.Text.Trim());
+                txtEmpleado.Text = "";
+                txtEmpleado.Focus();
+            }
+
+            if (radioBtnapaterno.Checked == true & radioBtnActivo.Checked == true)
+            {
+                fgridEmpleados(5, txtEmpleado.Text.Trim());
+                txtEmpleado.Text = "";
+                txtEmpleado.Focus();
+            }
+
+            if (radioBtnapaterno.Checked == true & radioBtnInactivo.Checked == true)
+            {
+                fgridEmpleados(6, txtEmpleado.Text.Trim());
+                txtEmpleado.Text = "";
+                txtEmpleado.Focus();
+            }
+            //fgridEmpleados(1, txtEmpleado.Text.Trim());
+            //txtEmpleado.Text = "";
+            //txtEmpleado.Focus();
+
+
+        }
+        //boton minimizar        
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        //boton cerrar
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("¿Seguro que desea salir?", "SIPAA", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (result == DialogResult.No)
+            {
+
+            }
+        }
+
+        private void Empleados_Load(object sender, EventArgs e)
+        {
+            //llama el tooltip
+            ftooltip();
+
+            //pone el foco en el campo de busqueda
+            txtEmpleado.Focus();
+
+            //llena grid
+            fgridEmpleados(1,"");
+        }
+
+        /////////funciones. FUNCION tooltip
+        private void ftooltip()
+        {
+            //crea tool tip
+            ToolTip toolTip1 = new ToolTip();
+
+            //configuracion
+            toolTip1.AutoPopDelay = 5000;
+            toolTip1.InitialDelay = 1000;
+            toolTip1.ReshowDelay = 500;
+            toolTip1.ShowAlways = true;
+
+            //configura texto del objeto
+            toolTip1.SetToolTip(this.btnCerrar, "Cierrar Sistema");
+            toolTip1.SetToolTip(this.btnMinimizar, "Minimizar Sistema");
+            toolTip1.SetToolTip(this.btnRegresar, "Regresar");
+            toolTip1.SetToolTip(this.btnBuscar, "Busca Registro");
+        }
+
+        //FUNCION que Llena el Grid de Empleados
+        private void fgridEmpleados(int popc, string pbusq)
+        {
+            DataTable dtempleados = contenedorempleados.obtenerempleados(popc, pbusq);            
+            dgvEmpleados.DataSource = dtempleados;
+
+            //dgvEmpleados.Columns[0].Visible = false;
+            //dgvEmpleados.Columns[1].Width = 355;
+            //dgvComp.Columns[2].Width = 125;
+            dgvEmpleados.ClearSelection();
+        }
+    }
+}
---
+++ SIPAA_CS/SIPAA_CS/Recursos_Humanos/Administracion/Mensajes.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using SIPAA_CS.Properties;
+using SIPAA_CS.Recursos_Humanos.App_Code;
+
+namespace SIPAA_CS.Recursos_Humanos.Administracion
+{
+
+    #region variables
+
+
+    #endregion
+
+    public partial class Mensajes : Form
+    {
+        public Mensajes()
+        {
+            InitializeComponent();
+        }
+
+        //se "instancia" la clase para usar todos los metodos que contenga
+        MensajesSonarh pantallaMensajes = new MensajesSonarh();
+                
+        private void Mensajes_Load(object sender, EventArgs e)
+        {
+            ftooltip();
+            txtMensaje.Focus();
+            gridMensajes(1, "");
+            
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            gridMensajes(1, txtMensaje.Text.Trim());
+            txtMensaje.Text = "";
+            txtMensaje.Focus();            
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("¿Seguro que desea salir?", "SIPAA", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (result == DialogResult.No)
+            {
+                
+            }
+        }
+
+        private void ftooltip()
+        {
+            //crea tool tip
+            ToolTip toolTip1 = new ToolTip();
+
+            //configuracion
+            toolTip1.AutoPopDelay = 5000;
+            toolTip1.InitialDelay = 1000;
+            toolTip1.ReshowDelay = 500;
+            toolTip1.ShowAlways = true;
+
+            //configura texto del objeto
+            toolTip1.SetToolTip(this.btnCerrar, "Cierrar Sistema");
+            toolTip1.SetToolTip(this.btnMinimizar, "Minimizar Sistema");
+            toolTip1.SetToolTip(this.btnRegresar, "Regresar");
+            toolTip1.SetToolTip(this.btnBuscar, "Busca Registro");
+        }
+
+        private void gridMensajes(int popc, string pbusqueda)
+        {
+            DataTable dtmensajes = pantallaMensajes.obtenermensajes(popc, pbusqueda);
+            dgvMensajes.DataSource = dtmensajes;
+            dgvMensajes.ClearSelection();
+        }
+        
+    }
+}
