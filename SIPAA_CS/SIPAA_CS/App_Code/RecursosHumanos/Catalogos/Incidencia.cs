005bbf7d52b99b1657ef24712cf6261986a507fe
mdupont <mdupont@DUPONT-PC>
2017-03-23 08:19:56 -0600

    Catalogo de Incidencias

--- SIPAA_CS/SIPAA_CS/App_Code/RecursosHumanos/Catalogos/Incidencia.cs
+++ SIPAA_CS/SIPAA_CS/App_Code/RecursosHumanos/Catalogos/Incidencia.cs
@@ -11,8 +11,11 @@ namespace SIPAA_CS.App_Code
 {
     class Incidencia
     {
+        //SE DECLARAN VARIABLES
+        public int p_opcion;
         public int CVIncidencia;
         public string Descripcion;
+        public int p_orden;
         public int CVRepresenta;
         public string Representa;
         public string UsuuMod;
@@ -20,6 +23,19 @@ namespace SIPAA_CS.App_Code
         public string PrguMod;
         public int TipoID;
         public string TipoIncidencia;
+        public int p_respuesta;
+
+        //CONTRUCTOR
+        public Incidencia()
+        {
+            p_opcion = 0;
+            CVIncidencia = 0;
+            Descripcion = "";
+            p_orden = 0;
+            UsuuMod = "";
+            PrguMod = "";
+            p_respuesta = 0;
+        }
 
 
         public DataTable ObtenerRepresentaxIncidencia(Incidencia objIncidencia,int Opcion) {
@@ -70,6 +86,75 @@ namespace SIPAA_CS.App_Code
             Adapter.Fill(dtIncidencia);
 
             return dtIncidencia;
+        }
+        //METODO FORMAS DE REGISTRO LLENA GRID MTD
+        public DataTable Incidencia_S(int p_opcion, int p_cvincidencia, string p_descripcion, int p_orden, string p_usuumod, string p_prgumodr)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = @"usp_rechincidencia_suid";
+            cmd.CommandType = CommandType.StoredProcedure;
+            Conexion objConexion = new Conexion();
+            objConexion.asignarConexion(cmd);
+
+            cmd.Parameters.Add("@p_opcion", SqlDbType.Int).Value = p_opcion;
+            cmd.Parameters.Add("@p_cvincidencia", SqlDbType.VarChar).Value = p_cvincidencia;
+            cmd.Parameters.Add("@p_descripcion", SqlDbType.VarChar).Value = p_descripcion;
+            cmd.Parameters.Add("@p_orden", SqlDbType.VarChar).Value = p_orden;
+            cmd.Parameters.Add("@p_usuumod", SqlDbType.VarChar).Value = p_usuumod;
+            cmd.Parameters.Add("@p_prgumod", SqlDbType.VarChar).Value = p_prgumodr;
+
+            objConexion.asignarConexion(cmd);
+            SqlDataAdapter Adapter = new SqlDataAdapter(cmd);
+            objConexion.cerrarConexion();
+
+            DataTable FormaReg_S = new DataTable();
+            Adapter.Fill(FormaReg_S);
+            return FormaReg_S;
+
+        }
+        //metodo actualizar, insertar, eliminar registro MTD
+        public int Incidencia_UID(int p_opcion, int p_cvincidencia, string p_descripcion, int p_orden, string p_usuumod, string p_prgumodr)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = @"usp_rechformareg_suid";
+            cmd.CommandType = CommandType.StoredProcedure;
+            Conexion objConexion = new Conexion();
+            objConexion.asignarConexion(cmd);
+
+            cmd.Parameters.Add("@p_opcion", SqlDbType.Int).Value = p_opcion;
+            cmd.Parameters.Add("@p_cvincidencia", SqlDbType.VarChar).Value = p_cvincidencia;
+            cmd.Parameters.Add("@p_descripcion", SqlDbType.VarChar).Value = p_descripcion;
+            cmd.Parameters.Add("@p_orden", SqlDbType.VarChar).Value = p_orden;
+            cmd.Parameters.Add("@p_usuumod", SqlDbType.VarChar).Value = p_usuumod;
+            cmd.Parameters.Add("@p_prgumod", SqlDbType.VarChar).Value = p_prgumodr;
+
+            objConexion.asignarConexion(cmd);
+
+            int iResponse = Convert.ToInt32(cmd.ExecuteScalar());
+            objConexion.cerrarConexion();
+            return iResponse;
+        }
+        //METODO FORMAS DE REGISTRO VALIDA CREA REGISTRO MTD
+        public int Incidencia_V(int p_opcion, int p_cvincidencia, string p_descripcion, int p_orden, string p_usuumod, string p_prgumodr)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = @"usp_rechformareg_suid";
+            cmd.CommandType = CommandType.StoredProcedure;
+            Conexion objConexion = new Conexion();
+            objConexion.asignarConexion(cmd);
+
+            cmd.Parameters.Add("@p_opcion", SqlDbType.Int).Value = p_opcion;
+            cmd.Parameters.Add("@p_cvincidencia", SqlDbType.VarChar).Value = p_cvincidencia;
+            cmd.Parameters.Add("@p_descripcion", SqlDbType.VarChar).Value = p_descripcion;
+            cmd.Parameters.Add("@p_orden", SqlDbType.VarChar).Value = p_orden;
+            cmd.Parameters.Add("@p_usuumod", SqlDbType.VarChar).Value = p_usuumod;
+            cmd.Parameters.Add("@p_prgumod", SqlDbType.VarChar).Value = p_prgumodr;
+
+            objConexion.asignarConexion(cmd);
+
+            int iResponse = Convert.ToInt32(cmd.ExecuteScalar());
+            objConexion.cerrarConexion();
+            return iResponse;
         }
 
     }
