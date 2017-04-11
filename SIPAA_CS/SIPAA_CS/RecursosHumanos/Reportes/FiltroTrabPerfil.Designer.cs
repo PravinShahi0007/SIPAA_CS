1c8d72661097f32d8a3ce38667be891c3f8b1dfc
mdupont <mdupont@DUPONT-PC>
2017-04-11 12:00:27 -0500

    reporte perfil trabajador

---
+++ SIPAA_CS/SIPAA_CS/RecursosHumanos/Reportes/FiltroTrabPerfil.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace SIPAA_CS.RecursosHumanos.Reportes
+{
+    partial class FiltroTrabPerfil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FiltroTrabPerfil));
+            System.Windows.Forms.Label lblubicacion;
+            System.Windows.Forms.Label lblidcia;
+            System.Windows.Forms.Label lblidtrab;
+            System.Windows.Forms.Label label9;
+            System.Windows.Forms.Label lblimprimir;
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.btnMinimizar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.pnlBusqueda = new System.Windows.Forms.Panel();
+            this.cboUbicacion = new System.Windows.Forms.ComboBox();
+            this.cboCia = new System.Windows.Forms.ComboBox();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.txtIdTrab = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            lblubicacion = new System.Windows.Forms.Label();
+            lblidcia = new System.Windows.Forms.Label();
+            lblidtrab = new System.Windows.Forms.Label();
+            label9 = new System.Windows.Forms.Label();
+            lblimprimir = new System.Windows.Forms.Label();
+            this.pnlBusqueda.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel2.Location = new System.Drawing.Point(38, 30);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(37, 41);
+            this.panel2.TabIndex = 149;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label5.Location = new System.Drawing.Point(5, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 23);
+            this.label5.TabIndex = 148;
+            this.label5.Text = "     Marco Dupont";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Location = new System.Drawing.Point(416, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 23);
+            this.label3.TabIndex = 147;
+            this.label3.Text = "Perfil de Empleados";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(87)))), ((int)(((byte)(155)))));
+            this.btnRegresar.BackgroundImage = global::SIPAA_CS.Properties.Resources.ic_reply_white_18dp;
+            this.btnRegresar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRegresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegresar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(87)))), ((int)(((byte)(155)))));
+            this.btnRegresar.Location = new System.Drawing.Point(943, 1);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(30, 24);
+            this.btnRegresar.TabIndex = 146;
+            this.btnRegresar.UseVisualStyleBackColor = false;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(87)))), ((int)(((byte)(155)))));
+            this.btnMinimizar.BackgroundImage = global::SIPAA_CS.Properties.Resources.ic_remove_white_18dp;
+            this.btnMinimizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimizar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(87)))), ((int)(((byte)(155)))));
+            this.btnMinimizar.Location = new System.Drawing.Point(972, 1);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(24, 24);
+            this.btnMinimizar.TabIndex = 145;
+            this.btnMinimizar.TabStop = false;
+            this.btnMinimizar.UseVisualStyleBackColor = false;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(67)))), ((int)(((byte)(54)))));
+            this.btnCerrar.BackgroundImage = global::SIPAA_CS.Properties.Resources.ic_clear_white_18dp;
+            this.btnCerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCerrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(67)))), ((int)(((byte)(54)))));
+            this.btnCerrar.Location = new System.Drawing.Point(998, 1);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(24, 24);
+            this.btnCerrar.TabIndex = 144;
+            this.btnCerrar.TabStop = false;
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // pnlBusqueda
+            // 
+            this.pnlBusqueda.Controls.Add(lblubicacion);
+            this.pnlBusqueda.Controls.Add(this.cboUbicacion);
+            this.pnlBusqueda.Controls.Add(lblidcia);
+            this.pnlBusqueda.Controls.Add(this.cboCia);
+            this.pnlBusqueda.Controls.Add(lblidtrab);
+            this.pnlBusqueda.Controls.Add(this.panel7);
+            this.pnlBusqueda.Controls.Add(this.txtIdTrab);
+            this.pnlBusqueda.Controls.Add(label9);
+            this.pnlBusqueda.Location = new System.Drawing.Point(38, 138);
+            this.pnlBusqueda.Name = "pnlBusqueda";
+            this.pnlBusqueda.Size = new System.Drawing.Size(477, 279);
+            this.pnlBusqueda.TabIndex = 151;
+            this.pnlBusqueda.TabStop = true;
+            // 
+            // lblubicacion
+            // 
+            lblubicacion.AutoSize = true;
+            lblubicacion.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblubicacion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(87)))), ((int)(((byte)(155)))));
+            lblubicacion.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            lblubicacion.Location = new System.Drawing.Point(41, 192);
+            lblubicacion.Name = "lblubicacion";
+            lblubicacion.Size = new System.Drawing.Size(54, 16);
+            lblubicacion.TabIndex = 60;
+            lblubicacion.Text = "Ubicación";
+            // 
+            // cboUbicacion
+            // 
+            this.cboUbicacion.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cboUbicacion.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cboUbicacion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
+            this.cboUbicacion.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboUbicacion.FormattingEnabled = true;
+            this.cboUbicacion.Location = new System.Drawing.Point(44, 213);
+            this.cboUbicacion.Name = "cboUbicacion";
+            this.cboUbicacion.Size = new System.Drawing.Size(291, 25);
+            this.cboUbicacion.TabIndex = 3;
+            this.cboUbicacion.Text = "Seleccionar ";
+            // 
+            // lblidcia
+            // 
+            lblidcia.AutoSize = true;
+            lblidcia.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblidcia.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(87)))), ((int)(((byte)(155)))));
+            lblidcia.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            lblidcia.Location = new System.Drawing.Point(41, 117);
+            lblidcia.Name = "lblidcia";
+            lblidcia.Size = new System.Drawing.Size(56, 16);
+            lblidcia.TabIndex = 58;
+            lblidcia.Text = "Compañia";
+            // 
+            // cboCia
+            // 
+            this.cboCia.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cboCia.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cboCia.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
+            this.cboCia.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboCia.FormattingEnabled = true;
+            this.cboCia.Location = new System.Drawing.Point(44, 137);
+            this.cboCia.Name = "cboCia";
+            this.cboCia.Size = new System.Drawing.Size(291, 25);
+            this.cboCia.TabIndex = 2;
+            this.cboCia.Text = "Seleccionar ";
+            // 
+            // lblidtrab
+            // 
+            lblidtrab.AutoSize = true;
+            lblidtrab.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblidtrab.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(87)))), ((int)(((byte)(155)))));
+            lblidtrab.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            lblidtrab.Location = new System.Drawing.Point(41, 49);
+            lblidtrab.Name = "lblidtrab";
+            lblidtrab.Size = new System.Drawing.Size(106, 16);
+            lblidtrab.TabIndex = 44;
+            lblidtrab.Text = "No de Trabajador";
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(119)))), ((int)(((byte)(189)))));
+            this.panel7.Location = new System.Drawing.Point(44, 87);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(250, 2);
+            this.panel7.TabIndex = 43;
+            // 
+            // txtIdTrab
+            // 
+            this.txtIdTrab.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
+            this.txtIdTrab.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtIdTrab.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdTrab.Location = new System.Drawing.Point(44, 67);
+            this.txtIdTrab.Name = "txtIdTrab";
+            this.txtIdTrab.Size = new System.Drawing.Size(250, 15);
+            this.txtIdTrab.TabIndex = 1;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label9.ForeColor = System.Drawing.Color.Gray;
+            label9.Image = ((System.Drawing.Image)(resources.GetObject("label9.Image")));
+            label9.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            label9.Location = new System.Drawing.Point(5, 0);
+            label9.Name = "label9";
+            label9.Size = new System.Drawing.Size(152, 17);
+            label9.TabIndex = 41;
+            label9.Text = "       Filtros de Reporte";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(lblimprimir);
+            this.panel1.Controls.Add(this.btnImprimir);
+            this.panel1.Location = new System.Drawing.Point(597, 138);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(399, 114);
+            this.panel1.TabIndex = 152;
+            // 
+            // lblimprimir
+            // 
+            lblimprimir.AutoSize = true;
+            lblimprimir.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblimprimir.ForeColor = System.Drawing.Color.Gray;
+            lblimprimir.Image = ((System.Drawing.Image)(resources.GetObject("lblimprimir.Image")));
+            lblimprimir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            lblimprimir.Location = new System.Drawing.Point(60, 48);
+            lblimprimir.Name = "lblimprimir";
+            lblimprimir.Size = new System.Drawing.Size(199, 17);
+            lblimprimir.TabIndex = 66;
+            lblimprimir.Text = "       Imprimir Perfil Trabajador";
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
+            this.btnImprimir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnImprimir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
+            this.btnImprimir.Image = global::SIPAA_CS.Properties.Resources.Imprimir;
+            this.btnImprimir.Location = new System.Drawing.Point(282, 32);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(50, 50);
+            this.btnImprimir.TabIndex = 4;
+            this.btnImprimir.Tag = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = false;
+            // 
+            // FiltroTrabPerfil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SIPAA_CS.Properties.Resources.f8;
+            this.ClientSize = new System.Drawing.Size(1024, 768);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlBusqueda);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnMinimizar);
+            this.Controls.Add(this.btnCerrar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FiltroTrabPerfil";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FiltroTrabPerfil";
+            this.Load += new System.EventHandler(this.FiltroTrabPerfil_Load);
+            this.pnlBusqueda.ResumeLayout(false);
+            this.pnlBusqueda.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.Button btnMinimizar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Panel pnlBusqueda;
+        private System.Windows.Forms.ComboBox cboUbicacion;
+        private System.Windows.Forms.ComboBox cboCia;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.TextBox txtIdTrab;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnImprimir;
+    }
+}
