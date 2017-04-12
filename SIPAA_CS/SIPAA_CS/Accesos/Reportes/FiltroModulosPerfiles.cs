bbd848a66351c380ac80a5d6da8510112db82142
JUSTOSIERRA\globatos <gamaliellobato@gmail.com>
2017-04-12 13:58:02 -0500

    Se agrega formularioa de FiltroModulosPerfiles

---
+++ SIPAA_CS/SIPAA_CS/Accesos/Reportes/FiltroModulosPerfiles.cs
@@ -0,0 +1,160 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using SIPAA_CS.App_Code;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SIPAA_CS.Accesos.Reportes
+{
+    public partial class FiltroModulosPerfiles : Form
+    {
+        public int modulo;
+        public int perfil;
+        //***********************************************************************************************
+        //Autor: Gamaliel Lobato Solis
+        //Fecha creación:dd-mm-aaaa       Última Modificacion: dd-mm-aaaa
+        //Descripción: -------------------------------
+        //***********************************************************************************************
+        public FiltroModulosPerfiles()
+        {
+            InitializeComponent();
+        }
+        //-----------------------------------------------------------------------------------------------
+        //                                      C O M B O S
+        //-----------------------------------------------------------------------------------------------
+        //-----------------------------------------------------------------------------------------------
+        //                                      G R I D // S
+        //-----------------------------------------------------------------------------------------------
+        //-----------------------------------------------------------------------------------------------
+        //                                     B O T O N E S
+        //-----------------------------------------------------------------------------------------------
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+
+        }
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("¿Seguro que deseas salir?", "SIPAA", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (result == DialogResult.No)
+            {
+
+            }
+        }
+
+        private void btnImprimirDetalle_Click(object sender, EventArgs e)
+        {
+            modulo = cbModulo.SelectedIndex;
+            perfil = cbPerfil.SelectedIndex;
+
+
+            //VALIDA SELECCION DE TODOS EN AMBOS COMBOS
+            if (modulo == 0 && perfil == 0 )
+            {
+                Utilerias.ControlNotificaciones(panelTag, lbMensaje, 3, "modulo perfil");
+                timer1.Start();
+
+                
+                Perfil objPerfil = new Perfil();
+                DataTable dtReporte;
+                dtReporte = objPerfil.ReportePerfilesModulos("",0,"","",0,0,0,0,0,6);
+
+                switch (dtReporte.Rows.Count)
+                {
+
+                    case 0:
+                        DialogResult result = MessageBox.Show("Consulta Sin Resultados", "SIPAA");
+                        break;
+
+                    default:
+                        ViewerReporteModulosPerfiles form = new ViewerReporteModulosPerfiles();
+                        ReporteModulosPerfiles dtrpt = new ReporteModulosPerfiles();
+                        ReportDocument ReportDoc = Utilerias.ObtenerObjetoReporte(dtReporte, "Accesos", dtrpt.ResourceName);
+
+                        //ReportDoc.SetParameterValue("TotalRegistros", dtReporte.Rows.Count.ToString());
+                        //ReportDoc.SetParameterValue("Filtro", cbEstatus.SelectedItem.ToString());
+                        form.RptDoc = ReportDoc;
+                        form.Show();
+                        break;
+
+                }
+            }
+
+
+        }
+        //-----------------------------------------------------------------------------------------------
+        //                           C A J A S      D E      T E X T O   
+        //-----------------------------------------------------------------------------------------------
+
+
+        //-----------------------------------------------------------------------------------------------
+        //                                     E V E N T O S
+        //-----------------------------------------------------------------------------------------------
+        private void FiltroModulosPerfiles_Load(object sender, EventArgs e)
+        {
+            //// Se crea lista de permisos por pantalla
+            //LoginInfo.dtPermisosTrabajador = Modulo.ObtenerPermisosxUsuario(LoginInfo.IdTrab);
+            //DataRow[] row = LoginInfo.dtPermisosTrabajador.Select("CVModulo = '" + this.Tag + "'");
+            //LoginInfo.ltPermisosPantalla = Utilerias.CrearListaPermisoxPantalla(row, LoginInfo.ltPermisosPantalla);
+            ////////////////////////////////////////////////////////
+            //// resize 
+            Utilerias.ResizeForm(this, Utilerias.PantallaSistema());
+            /////////////////////////////////////////////////////////////////////////////////////////////////////
+            //// variables de permisos
+            //Permisos.Crear = Utilerias.ControlPermiso("Crear", LoginInfo.ltPermisosPantalla);
+            //Permisos.Actualizar = Utilerias.ControlPermiso("Actualizar", LoginInfo.ltPermisosPantalla);
+            //Permisos.Eliminar = Utilerias.ControlPermiso("Eliminar", LoginInfo.ltPermisosPantalla);
+            //Permisos.Imprimir = Utilerias.ControlPermiso("Imprimir", LoginInfo.ltPermisosPantalla);
+            //////////////////////////////////////////////////////////////////////////////////////////
+
+            Perfil objPerfil = new Perfil();
+            Modulo objModulo = new Modulo();
+
+            DataTable dtPerfil = objPerfil.ObtenerPerfiles("", "", "", 10);
+            DataTable dtModulo = objModulo.ReporteModulos("","","",0,"","","",0,"","",29);
+
+            llenaCombo(cbPerfil, dtPerfil, "cvperfil", "Descripcion");
+            llenaCombo(cbModulo, dtModulo, "cvmodulo", "descripcion");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panelTag.Visible = false;
+            timer1.Stop();
+        }
+        //-----------------------------------------------------------------------------------------------
+        //                                      F U N C I O N E S 
+        //-----------------------------------------------------------------------------------------------
+        public static void llenaCombo(ComboBox cb, DataTable dt, string sClave, string sDescripcion)
+        {
+            DataRow row = dt.NewRow();
+            row[sClave] = "0";
+            row[sDescripcion] = "Todos";
+            dt.Rows.InsertAt(row, 0);
+            cb.DataSource = dt;
+            cb.DisplayMember = sDescripcion;
+            cb.ValueMember = sClave;
+        }
+       
+        //-----------------------------------------------------------------------------------------------
+        //                                      R E P O R T E
+        //-----------------------------------------------------------------------------------------------
+
+    }
+}
---
+++ SIPAA_CS/SIPAA_CS/Accesos/Reportes/ViewerReporteModulosPerfiles.cs
@@ -0,0 +1,97 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using SIPAA_CS.App_Code;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SIPAA_CS.Accesos.Reportes
+{
+    public partial class ViewerReporteModulosPerfiles : Form
+    {
+        public ReportDocument RptDoc;
+        //***********************************************************************************************
+        //Autor: Gamaliel Lobato Solis
+        //Fecha creación:dd-mm-aaaa       Última Modificacion: dd-mm-aaaa
+        //Descripción: -------------------------------
+        //***********************************************************************************************
+        public ViewerReporteModulosPerfiles()
+        {
+            InitializeComponent();
+        }
+        //-----------------------------------------------------------------------------------------------
+        //                                      C O M B O S
+        //-----------------------------------------------------------------------------------------------
+        //-----------------------------------------------------------------------------------------------
+        //                                      G R I D // S
+        //-----------------------------------------------------------------------------------------------
+        //-----------------------------------------------------------------------------------------------
+        //                                     B O T O N E S
+        //-----------------------------------------------------------------------------------------------
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("¿Seguro que deseas salir?", "SIPAA", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (result == DialogResult.No)
+            {
+
+            }
+        }
+        //-----------------------------------------------------------------------------------------------
+        //                           C A J A S      D E      T E X T O   
+        //-----------------------------------------------------------------------------------------------
+
+
+        //-----------------------------------------------------------------------------------------------
+        //                                     E V E N T O S
+        //-----------------------------------------------------------------------------------------------
+        private void ViewerReporteModulosPerfiles_Load(object sender, EventArgs e)
+        {
+            //// Se crea lista de permisos por pantalla
+            //LoginInfo.dtPermisosTrabajador = Modulo.ObtenerPermisosxUsuario(LoginInfo.IdTrab);
+            //DataRow[] row = LoginInfo.dtPermisosTrabajador.Select("CVModulo = '" + this.Tag + "'");
+            //LoginInfo.ltPermisosPantalla = Utilerias.CrearListaPermisoxPantalla(row, LoginInfo.ltPermisosPantalla);
+            ////////////////////////////////////////////////////////
+            //// resize 
+            Utilerias.ResizeForm(this, Utilerias.PantallaSistema());
+            /////////////////////////////////////////////////////////////////////////////////////////////////////
+            //// variables de permisos
+            //Permisos.Crear = Utilerias.ControlPermiso("Crear", LoginInfo.ltPermisosPantalla);
+            //Permisos.Actualizar = Utilerias.ControlPermiso("Actualizar", LoginInfo.ltPermisosPantalla);
+            //Permisos.Eliminar = Utilerias.ControlPermiso("Eliminar", LoginInfo.ltPermisosPantalla);
+            //Permisos.Imprimir = Utilerias.ControlPermiso("Imprimir", LoginInfo.ltPermisosPantalla);
+            //////////////////////////////////////////////////////////////////////////////////////////
+            ReporteView.ReportSource = RptDoc;
+        }
+        
+        //-----------------------------------------------------------------------------------------------
+        //                                      F U N C I O N E S 
+        //-----------------------------------------------------------------------------------------------
+
+
+
+        //-----------------------------------------------------------------------------------------------
+        //                                      R E P O R T E
+        //-----------------------------------------------------------------------------------------------
+
+
+    }
+}
